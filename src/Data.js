caa465c3518de9b9930e542ec3b8d3d338010787
sekem12345 <akinolaitunu12345@gmail.com>
2024-08-30 13:26:01 +0100

    we move

---
+++ src/Data.js
@@ -0,0 +1,17 @@
+export const API_KEY = 'AIzaSyA6uxlL-IEj2tXWPMP2krZwoNo_vENHgSc';
+
+
+ export const value_converter = (value) => {
+    if(value>=1000000)
+    {
+      return Math.floor(value/100000)+"M";
+    }
+    else if(value>=1000)
+    {
+      return Math.floor(value/1000)+"K";
+      
+    }
+    else{
+      return value;
+    }
+}
--- src/placea/Video/Video.jsx
+++ src/placea/Video/Video.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Video.css'
 import Playvideo from '../../Components/PlayVideo/Playvideo'
+import Recommened from '../../Components/Recommened/Recommened'
 
 const Video = () => {
   return (
     <div className='play-container'>
           <Playvideo/>
+          <Recommened/>
     </div>
   )
 }
